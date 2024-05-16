5f1b5da0e4e50f1ab7681002bdbb562423e38064
snonow <arnowilhelm3@icloud.com>
2024-05-16 11:43:11 +0200

    "Updated session variable and navigation links in PHP files"

--- Web/src/components/navbar/navbar.php
+++ Web/src/components/navbar/navbar.php
@@ -6,8 +6,12 @@
     </div>
 
     <div class="links">
-        <a href="" class="nav-link ">Accueil</a>
-        <a href="" class="nav-link ">Nos livres</a>
-        <a href="" class="nav-link ">Accès - Contact</a>
+        <a href="welcome.php" class="nav-link">Accueil</a>
+        <a href="" class="nav-link">Nos livres</a>
+        <a href="" class="nav-link">Accès - Contact</a>
+    </div>
+
+    <div id="login_signin">
+        <a href="Web/src/pages/login.php" class="nav-link">Log in / Sign in</a>
     </div>
 </div>
