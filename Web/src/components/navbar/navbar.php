69271a8de9d8655b4aefba8b81dbd9b77129fe3b
Baptiste <133878148+BPTGRND@users.noreply.github.com>
2024-05-21 18:36:43 +0200

    Update navbar.php

--- Web/src/components/navbar/navbar.php
+++ Web/src/components/navbar/navbar.php
@@ -37,9 +37,23 @@ function getUserInfo($conn, $user_id) {
             // Check if a `user_id` is passed in the URL
             if (isset($_SESSION['user_id'])) {
 
-                echo '<div id="profile">';
-                echo '<a href="./livres.php?pages=profile" class="nav-link">Mon compte</a>';
-                echo '</div>';
+
+
+                $user_id = intval($_SESSION['user_id']);
+
+                // Call the function to get user information
+                $user_info = getUserInfo($conn, $user_id);
+
+                $photo_de_profil = htmlspecialchars($user_info['photo_de_profil']);
+
+                if ($photo_de_profil == NULL) {
+                    $photo_de_profil = "pictures/blank-profile.png";
+                }
+
+                // Output the profile picture as a clickable link to the user's profile
+                echo '<a href="./livres.php?pages=profile&user_id=' . $user_id . '" class="profile-picture">';
+                echo '<img src="' . $photo_de_profil . '" alt="Profile Picture" id="profil">';
+                echo '</a>';
 
             } else {
                 // If no `user_id` is provided, display login and signup links
