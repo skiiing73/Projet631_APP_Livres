ddb3e888dad566941f28c91742e21b3f68191b26
BPTGRND <baptiste.renand@gmx.com>
2024-05-21 17:39:25 +0200

    modif

--- Web/src/pages/new_welcome.php
+++ Web/src/pages/new_welcome.php
@@ -2,11 +2,7 @@
 <html lang="fr">
 
 <?php
-// Session 
-$_SESSION['page'] = "welcome";
-// Les fonctions importés : 
-$chemin = "./src/requests/table_welcome.php";
-require_once($chemin);
+require_once("./src/requests/table_welcome.php");
 ?>
 
 <head>
@@ -24,16 +20,6 @@ require_once($chemin);
     ?>
 
     <div class="welcome_page">
-    <?php
-        // Vérifier si l'utilisateur est déjà connecté, si oui le rediriger vers la page d'accueil
-        if(isset($_SESSION['user_id'])) {
-            // Récupérer le prénom de l'utilisateur depuis la session
-            $first_name = getUserFirstName($conn, $user_id);
-            // Afficher le message de bienvenue
-            echo "<p>Bonjour $first_name, bienvenue sur SuperLivres !</p>";
-        }
-        exit();
-    ?>
     </div>
 
 </body>
