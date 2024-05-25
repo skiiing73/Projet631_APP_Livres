6bc98d91b332be7d871d0e5f42e332b2ab8cf961
snonow <arnowilhelm3@icloud.com>
2024-05-25 20:02:11 +0200

    "Removed conditional redirect logic and merged conflicting changes in livres.php"

--- Web/livres.php
+++ Web/livres.php
@@ -1,13 +1,5 @@
 <?php
-<<<<<<< HEAD
-=======
 
-if (!isset($_GET['id_utilisateur']) && !isset($_GET['pages'])) {
-    header("Location: ./livres.php?pages=login");
-    exit();
-}
-
->>>>>>> origin/Web
 require_once("./lib/database.php");
 
 
